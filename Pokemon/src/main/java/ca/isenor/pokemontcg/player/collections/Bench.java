72e6cb6f13cbeb8a77c7fb2f8710393a48e5fcc1
David Isenor <d.isenor@dal.ca>
2017-06-05 17:45:05 -0400

    Update bench class
    Add toString method for easy printing. Update comments. Update add
    method for future use. Add size method.
    
    Signed-off-by: David Isenor <d.isenor@dal.ca>

--- Pokemon/src/main/java/ca/isenor/pokemontcg/player/collections/Bench.java
+++ Pokemon/src/main/java/ca/isenor/pokemontcg/player/collections/Bench.java
@@ -5,16 +5,14 @@ import java.util.LinkedList;
 
 import ca.isenor.pokemontcg.cards.pokemon.Pokemon;
 /**
- * The benched pokemon that a trainer has access to.
- * <p> Initial max size of 5 pokemon.
+ * The benched Pokemon that a trainer has access to.
+ * <p> Initial max size of 5 Pokemon.
  * @author dawud
  *
  */
 public class Bench implements Serializable {
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = -4685486583007035960L;
+
 	private LinkedList<Pokemon> bench;
 	private int maxSize;
 
@@ -23,14 +21,49 @@ public class Bench implements Serializable {
 		maxSize = 5;
 	}
 
-	public void add(Pokemon pokemon) {
+	/**
+	 * Add a Pokemon to the player's bench. This method checks the max hand size before adding.
+	 *
+	 * @param pokemon
+	 * @return true if the Pokemon was added to the bench; false otherwise
+	 */
+	public boolean add(Pokemon pokemon) {
 		if (bench.size() < maxSize) {
-			bench.add(pokemon);
+			return bench.add(pokemon);
 		}
+		return false;
 	}
 
-	//figure out a way to determine how to choose an element from the list
+	/**
+	 * Removes and gets the Pokemon at index in the bench
+	 *
+	 * @param index
+	 * @return the selected Pokemon
+	 */
 	public Pokemon remove(int index) {
 		return bench.remove(index);
+	}
+
+	/**
+	 *
+	 * @return the number of Pokemon on the bench
+	 */
+	public int size() {
+		return bench.size();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		if (bench.isEmpty()) {
+			return "0: -----";
+		}
+		for (int i = 1; i <= bench.size(); i++) {
+			str.append(i + ": " + bench.get(i-1));
+			if (i != bench.size()) {
+				str.append("\n");
+			}
+		}
+		return str.toString();
 	}
 }
