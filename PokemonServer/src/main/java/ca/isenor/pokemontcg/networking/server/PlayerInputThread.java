aa884764b7fa49b520f452d99f3ecfe6e9ecfb30
David Isenor <d.isenor@dal.ca>
2017-06-09 20:39:26 -0300

    #7 Add new commands for opponent
    New commands were added to view opponent data such as hand size, deck
    size, number of prize cards, active pokemon, bench. Also added a deck
    and prizes command for the 'home' player.
    
    Signed-off-by: David Isenor <d.isenor@dal.ca>

--- PokemonServer/src/main/java/ca/isenor/pokemontcg/networking/server/PlayerInputThread.java
+++ PokemonServer/src/main/java/ca/isenor/pokemontcg/networking/server/PlayerInputThread.java
@@ -1,7 +1,7 @@
 package ca.isenor.pokemontcg.networking.server;
 import java.io.IOException;
 
-import ca.isenor.pokemontcg.cards.pokemon.Pokemon;
+import ca.isenor.pokemontcg.player.Player;
 
 /**
  * Handles commands sent from the client/player concurrently with turn operations.
@@ -14,6 +14,8 @@ import ca.isenor.pokemontcg.cards.pokemon.Pokemon;
 public class PlayerInputThread extends Thread {
 	private PlayerTurnController controller;
 	private int playerNumber;
+	private ServerPlayerThread thisPlayer;
+	private ServerPlayerThread otherPlayer;
 
 	public PlayerInputThread(PlayerTurnController controller, int playerNumber) {
 		this.controller = controller;
@@ -22,8 +24,6 @@ public class PlayerInputThread extends Thread {
 
 	@Override
 	public void run() {
-		ServerPlayerThread thisPlayer;
-		ServerPlayerThread otherPlayer;
 
 		// Deduce relative players
 		if (playerNumber == 0) {
@@ -39,39 +39,49 @@ public class PlayerInputThread extends Thread {
 			boolean finished = false;
 			while (!finished && (message = thisPlayer.getIn().readLine()) != null) {
 				System.out.println("Player" + playerNumber + ": \"" + message + "\"");
+				Player player = controller.getPlayer(playerNumber);
 				if (message.startsWith("chat")) {
-					otherPlayer.getOut().println(controller.getPlayer(playerNumber).getName() + ": " + message.substring(5));
+					otherPlayer.getOut().println(player.getName() + ": " + message.substring(5));
+				}
+				else if (message.startsWith("opp")) {
+					oppCommands(message.substring(4));
 				}
 				else if (message.startsWith("quit")) {
-					otherPlayer.getOut().println(controller.getPlayer(playerNumber).getName() + " has left the game.");
+					otherPlayer.getOut().println(player.getName() + " has left the game.");
 					finished = true;
 					System.out.println("Ending player input thread for Player" + playerNumber);
 				}
 				else if ("clear".equals(message)) {
 					thisPlayer.getOut().println("clearscreen");
 				}
+				else if ("deck".equals(message)) {
+					thisPlayer.getOut().println("\nYou have " + player.getDeck().size() + " cards left in your deck.");
+					System.out.println("Player" + playerNumber + " counted the cards in his/her deck.");
+				}
+				else if ("prizes".equals(message)) {
+					thisPlayer.getOut().println("\nYou have " + player.getPrizeCards().size() + " prize cards left.");
+					System.out.println("Player" + playerNumber + " counted his/her prize cards.");
+				}
 				else if ("bench".equals(message)) {
 					thisPlayer.getOut().println("multiline");
-					thisPlayer.getOut().println(controller.getPlayer(playerNumber).getBench());
+					thisPlayer.getOut().println(player.getBench());
 					thisPlayer.getOut().println("complete");
 					System.out.println("Player" + playerNumber + " looked at his/her bench.");
 				}
 				else if ("hand".equals(message)) {
 					thisPlayer.getOut().println("multiline");
-					thisPlayer.getOut().println(controller.getPlayer(playerNumber).getHand());
+					thisPlayer.getOut().println(player.getHand());
 					thisPlayer.getOut().println("complete");
 					System.out.println("Player" + playerNumber + " looked at his/her hand.");
 				}
 				else if ("active".equals(message)) {
 					thisPlayer.getOut().println("multiline");
-					Pokemon active = controller.getPlayer(playerNumber).getActive();
-					thisPlayer.getOut().println("Active Pokemon:\n" + active.longDescription());
+					thisPlayer.getOut().println("Active Pokemon:\n" + player.getActive().longDescription());
 					thisPlayer.getOut().println("complete");
 					System.out.println("Player" + playerNumber + " looked at his/her active Pokemon.");
 				}
 				else if ("myturn".equals(message)) {
-					int otherPlayerNumber = (playerNumber + 1) % 2;
-					String whosTurn = "No. It is " + controller.getPlayer(otherPlayerNumber).getName() + "'s turn.";
+					String whosTurn = "No. It is " + controller.getOpponentOf(playerNumber).getName() + "'s turn.";
 					if (controller.getPlayerTurn() == playerNumber) {
 						whosTurn = "Yes. It is your turn";
 					}
@@ -83,7 +93,7 @@ public class PlayerInputThread extends Thread {
 				}
 				else {
 					System.out.println("Unable to perform command " + message +
-							" for " + controller.getPlayer(playerNumber).getName());
+							" for " + player.getName());
 					thisPlayer.getOut().println("Unable to perform the command: " + message);
 				}
 			}
@@ -91,5 +101,31 @@ public class PlayerInputThread extends Thread {
 			e.printStackTrace();
 		}
 		thisPlayer.getOut().println("quit");
+	}
+
+	private void oppCommands(String message) {
+		Player opp = controller.getOpponentOf(playerNumber);
+		if ("bench".equals(message)) {
+			thisPlayer.getOut().println("\n" + opp.getName() + "'s bench:");
+			thisPlayer.getOut().println(opp.getBench());
+			System.out.println("Player" + playerNumber + " looked at his/her opponents bench.");
+		}
+		else if ("active".equals(message)) {
+			thisPlayer.getOut().println("\n" + opp.getName() + "'s active Pokemon:");
+			thisPlayer.getOut().println(opp.getActive().longDescription());
+			System.out.println("Player" + playerNumber + " looked at his/her opponents active Pokemon.");
+		}
+		else if ("hand".equals(message)) {
+			thisPlayer.getOut().println("\n" + opp.getName() + "'s hand has " + opp.getHand().size() + " cards in it.");
+			System.out.println("Player" + playerNumber + " counted the cards in his/her opponent's hand.");
+		}
+		else if ("prizes".equals(message)) {
+			thisPlayer.getOut().println("\n" + opp.getName() + " has " + opp.getPrizeCards().size() + " prize cards left.");
+			System.out.println("Player" + playerNumber + " counted his/her opponent's prize cards.");
+		}
+		else if ("deck".equals(message)) {
+			thisPlayer.getOut().println("\n" + opp.getName() + "'s deck has " + opp.getDeck().size() + " cards left.");
+			System.out.println("Player" + playerNumber + " counted the cards in his/her opponent's deck.");
+		}
 	}
 }
